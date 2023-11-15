9c9b6117ec13f9b49c39530e626392675a984b46
Jonas Norlinder <jonas@norlinder.nu>
2023-11-15 13:55:45 +0100

    dev

---
+++ .devcontainer/init.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+SCRIPT_PATH=`dirname $0 | while read a; do cd $a && pwd && break; done`
+echo $SCRIPT_PATH
+cd ${SCRIPT_PATH}/..
+
+bash configure --with-boot-jdk=/home/vscode/jdk-21.0.1/ --with-native-debug-symbols=internal --disable-warnings-as-errors 
+bash configure --with-boot-jdk=/home/vscode/jdk-21.0.1/ --with-debug-level=slowdebug --with-native-debug-symbols=internal --disable-warnings-as-errors
+
+exit
